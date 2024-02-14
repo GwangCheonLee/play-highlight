dc274fbb764649b9b214e25d2472523f1771f7c5
gclee <raitmid56@gmail.com>
2024-02-15 02:25:45 +0900

    feat: add Routes root,signIn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
+import Root from "./pages/Root/Root";
+import SignIn from "./pages/SignIn/SignIn";
+import {rootPath, signInPath} from "./common/routers/path";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-            </header>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path={rootPath} element={<Root/>}></Route>
+                <Route path={signInPath} element={<SignIn/>}></Route>
+                <Route path="*" element={<Navigate to="/" replace/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 

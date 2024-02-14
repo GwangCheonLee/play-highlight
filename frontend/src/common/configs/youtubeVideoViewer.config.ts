1d1c88b745c405c2c438552eda89230d45d3f7b7
gclee <raitmid56@gmail.com>
2024-02-14 19:30:50 +0900

    refactor: add interface youtubeVideoInformation

--- frontend/src/common/configs/youtubeVideoViewer.config.ts
+++ frontend/src/common/configs/youtubeVideoViewer.config.ts
@@ -1,13 +1,19 @@
 import {YouTubeEvent} from "react-youtube";
 
-const youtubeVideoInformation = [
+interface youtubeVideoInformationInterface {
+    id: string,
+    startTime: number,
+    description: string
+}
+
+const youtubeVideoInformation: youtubeVideoInformationInterface[] = [
     {id: 'ThI_ywSSsIQ', startTime: 0, description: "league of legends"},
     {id: 'JHcDUSeWN3Q', startTime: 0, description: "league of legends"},
     {id: "4DuSbbK_aKE", startTime: 0, description: "league of legends"},
     {id: "QdLyhBUIbts", startTime: 0, description: "league of legends"},
     {id: "j9YXi0WohA4", startTime: 0, description: "Overwatch2"}
 ]
-export const pickYoutubeVideoInformation = () => {
+export const pickYoutubeVideoInformation = (): youtubeVideoInformationInterface => {
     const randomNumber = Math.floor(Math.random() * (youtubeVideoInformation.length))
     return youtubeVideoInformation[randomNumber]
 }

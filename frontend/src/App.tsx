415c9376c8d782873698639813a1bff4da7e75d6
gclee <raitmid56@gmail.com>
2024-02-15 13:21:56 +0900

    feat: add route path signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import Root from "./pages/Root/Root";
 import SignIn from "./pages/SignIn/SignIn";
-import {rootPath, signInPath} from "./common/routers/path";
+import SignUp from "./pages/SignUp/SignUp";
+import {rootPath, signInPath, signUpPath} from "./common/routers/path";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path={rootPath} element={<Root/>}></Route>
+                <Route path={signUpPath} element={<SignUp/>}></Route>
                 <Route path={signInPath} element={<SignIn/>}></Route>
                 <Route path="*" element={<Navigate to="/" replace/>}/>
             </Routes>

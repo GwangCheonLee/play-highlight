bec115b673ee95f525c3f4821f7c4740c37a4f31
gclee <raitmid56@gmail.com>
2024-02-26 17:33:02 +0900

    feat: add endpoint post videos

--- backend/src/videos/videos.controller.ts
+++ backend/src/videos/videos.controller.ts
@@ -1,8 +1,20 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Query,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { VideosService } from './videos.service';
 import { FindVideosDto } from './dto/findVideos.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { GuardTypeEnum } from '../authentication/strategy/guard-type.enum';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as multer from 'multer';
+import { RequestByUser } from '../common/decorator/request-by-user.decorator';
+import { Users } from '../authentication/entities/users.entity';
 
 @UseGuards(AuthGuard(GuardTypeEnum.JWT_ACCESS))
 @Controller('api/videos')
@@ -19,6 +31,19 @@ export class VideosController {
         videos,
         nextCursor,
       },
+    };
+  }
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('video', { storage: multer.memoryStorage() }),
+  )
+  async uploadVideo(
+    @RequestByUser() user: Users,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    return {
+      data: { video: await this.videosService.saveVideo(user, file) },
     };
   }
 }

98b89b9216d2a15a54c54232ebd35f55f247dfc8
gclee <raitmid56@gmail.com>
2024-02-26 22:20:33 +0900

    feat: add GlobalStyle component

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './common/assets/fonts/Pretendard-Bold.woff'
+import './common/assets/fonts/Pretendard-Bold.otf'
+import './common/assets/fonts/Pretendard-Light.otf'
+import './common/assets/fonts/Pretendard-Light.woff'
+import './common/assets/fonts/Pretendard-Regular.otf'
+import './common/assets/fonts/Pretendard-Regular.woff'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {GlobalStyle} from "./common/components/GlobalStyle";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
+        <GlobalStyle/>
         <App/>
     </React.StrictMode>
 );

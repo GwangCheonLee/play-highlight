a0ebe48f359973985b4ffba657f0bac81dbb1234
gclee <raitmid56@gmail.com>
2024-02-26 16:32:58 +0900

    feat: import ffmpeg module

--- backend/src/videos/videos.module.ts
+++ backend/src/videos/videos.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { VideosController } from './videos.controller';
 import { VideosService } from './videos.service';
 import { VideosRepository } from './repositories/videos.repository';
+import { FfmpegModule } from '../ffmpeg/ffmpeg.module';
 
 @Module({
+  imports: [FfmpegModule],
   controllers: [VideosController],
   providers: [VideosService, VideosRepository],
 })

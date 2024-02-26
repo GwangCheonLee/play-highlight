4927f79a158468a0d0f25bb280d21d87120808bc
gclee <raitmid56@gmail.com>
2024-02-26 22:31:56 +0900

    feat: add function get video list

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import Header from "../../common/components/Header";
-import styled from "styled-components";
-import VideoCard from "./components/VideoCard";
+import React, {useEffect, useRef, useState} from 'react';
+import Header from '../../common/components/Header';
+import styled from 'styled-components';
+import VideoCard from './components/VideoCard';
+import NoVideos from './NoVideos';
+import {videoDetails} from '../../common/types/api/videos/videoTypes';
+import {fetchFindVideos} from "../../common/services/videos/videosService";
 
 const Main = styled.main`
     padding: 0 15%;
     height: calc(100vh - 69px);
-    overflow: hidden;
+    overflow: auto;
 
     @media (max-width: 640px) {
         padding: 0;
     }
-`
+`;
 
 const Section = styled.section`
     display: flex;
-    overflow: auto;
+    overflow: hidden;
     flex-wrap: wrap;
     padding: 20px 0;
     -ms-overflow-style: none;
@@ -23,31 +26,77 @@ const Section = styled.section`
 
     &::-webkit-scrollbar {
         display: none;
-
     }
-`
-
-const date = new Date()
-date.setSeconds(date.getSeconds() - 1)
+`;
 
 const Home = () => {
-    const arr = []
-    for (let i = 0; i < 5; i++) {
-        arr.push(<VideoCard videoId={"Asd"} alt={"Asd"}
-                            src={"https://i.ytimg.com/vi/x-NtIC3e0Ds/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAdQsCI5B3RtPUJn0Put9MWOhu83g"}
-                            nickname={"asd"} createdAt={date} email={"asd"}/>)
-    }
+    const [videos, setVideos] = useState<videoDetails[]>([]);
+    const [cursor, setCursor] = useState<number>(1);
+    const [limit, setLimit] = useState<number>(25);
+    const [nextCursor, setNextCursor] = useState<number | null>(null);
+    const sentinelRef = useRef<HTMLDivElement>(null);
+
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting && nextCursor) {
+                setCursor(prevCursor => prevCursor !== nextCursor ? nextCursor : prevCursor);
+            }
+        });
+
+        if (sentinelRef.current) {
+            observer.observe(sentinelRef.current);
+        }
+
+        return () => {
+            if (sentinelRef.current) {
+                observer.disconnect();
+            }
+        };
+    }, [nextCursor]);
+
+    useEffect(() => {
+        const fetchVideos = async () => {
+            try {
+                const {videos: newVideos, nextCursor: newNextCursor} = await fetchFindVideos({cursor, limit});
+                if (newVideos.length > 0) {
+                    setVideos(prevVideos => [...prevVideos, ...newVideos]);
+                    setNextCursor(newNextCursor);
+                }
+            } catch (error) {
+                console.error('Failed to fetch videos', error);
+            }
+        };
+
+        fetchVideos();
+    }, [cursor, limit]);
+
+
     return (
         <>
-
             <Header/>
             <Main>
                 <Section>
-                    {arr}
+                    {videos.length > 0 ? (
+                        videos.map((video) => (
+                            <VideoCard
+                                key={video.id}
+                                videoId={video.id.toString()}
+                                alt={`Video by ${video.user.nickname}`}
+                                src={`/videos/${video.thumbnailPath}`}
+                                nickname={video.user.nickname}
+                                createdAt={new Date(video.createdAt)}
+                                email={video.user.email}
+                            />
+                        ))
+                    ) : (
+                        <NoVideos/>
+                    )}
+                    <div ref={sentinelRef}/>
                 </Section>
             </Main>
         </>
     );
-}
+};
 
 export default Home;

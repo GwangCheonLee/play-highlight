8f17a968653d6ea14e6b99f93217e00b8bc6c64c
gclee <raitmid56@gmail.com>
2024-02-26 17:30:56 +0900

    feat: add function fetchUploadVideos

--- frontend/src/common/services/videos/videosService.ts
+++ frontend/src/common/services/videos/videosService.ts
@@ -7,3 +7,16 @@ export const fetchFindVideos = async (query: fetchFindVideosQuery, accessToken: 
     const response: AxiosResponse<fetchFindVideosResponse, any> = await axios.get(`${apiHost}/api/videos?cursor=${query.cursor}&limit=${query.limit}`, {headers: {Authorization: `Bearer ${accessToken}`}});
     return response.data.data
 };
+
+
+export const fetchUploadVideos = async (formData: FormData, accessToken: string) => {
+    const response: AxiosResponse<any, any> = await axios.post(`${apiHost}/api/videos`, formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            Authorization: `Bearer ${accessToken}`
+        }
+    });
+    return response.data.data
+};
+
+

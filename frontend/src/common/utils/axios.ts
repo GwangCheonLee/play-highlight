937e02fd976c31970e6d6c989a86348531fdb555
gclee <raitmid56@gmail.com>
2024-02-27 20:02:55 +0900

    refactor: remove environment variable BACKEND_HOST

--- frontend/src/common/utils/axios.ts
+++ frontend/src/common/utils/axios.ts
@@ -3,7 +3,7 @@ import {fetchAccessToken} from "../services/authentication/authenticationService
 import {logout, parseJwt} from "../constatns";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_HOST
+    baseURL: window.location.origin
 });
 
 api.interceptors.request.use(

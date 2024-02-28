e4efa21407d5da803a19dac6d767653fcb4fff4b
gclee <raitmid56@gmail.com>
2024-02-28 19:37:49 +0900

    feat: add video, auth reducer

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,9 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit';
 import modalReducer from "../features/modal/modalSlice";
+import videoReducer from "../features/video/videoSlice";
+import authReducer from "../features/auth/authSlice";
 
 export const index = configureStore({
     reducer: {
         modal: modalReducer,
+        video: videoReducer,
+        auth: authReducer
     },
 });
 

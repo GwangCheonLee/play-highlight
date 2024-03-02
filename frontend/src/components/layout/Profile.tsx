154562872503c1bc4c0cdcb48adfaf4cf4799a69
gclee <raitmid56@gmail.com>
2024-03-03 04:02:45 +0900

    migration: add Profile component

---
+++ frontend/src/components/layout/Profile.tsx
@@ -0,0 +1,13 @@
+import styled from "./profile.module.scss";
+import Link from "next/link";
+import { signInPath } from "@/common/routes/constants";
+
+export default function Profile() {
+  return (
+    <div className={styled.profile}>
+      <Link className={styled.signInButton} href={signInPath}>
+        Sign in
+      </Link>
+    </div>
+  );
+}

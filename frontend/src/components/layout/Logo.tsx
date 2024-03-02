fa10e4862be033b225c1d68c87492a485b1a7fa4
gclee <raitmid56@gmail.com>
2024-03-03 04:01:37 +0900

    migration: rename logo component

--- frontend/src/components/layout/Logo.tsx
+++ frontend/src/components/layout/Logo.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
-import styles from "./logo.module.css";
+import styles from "./logo.module.scss";
 import Image from "next/image";
+import { rootPath } from "@/common/routes/constants";
 
 export default function Logo() {
   return (
     <h1 className={styles.title}>
-      <Link className={styles.link} href={""}>
+      <Link className={styles.link} href={rootPath}>
         <Image
           className={styles.img}
           src={"/assets/images/logo.png"}

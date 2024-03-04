0731fd02ca931cd3026b555f93546c489871fe93
gclee <raitmid56@gmail.com>
2024-03-05 02:01:58 +0900

    migration: add signUp page

---
+++ frontend/src/app/(sign)/sign-up/SignUpForm.tsx
@@ -0,0 +1,69 @@
+"use client";
+import React from "react";
+import styles from "@/app/(sign)/sign.module.scss";
+import { useForm } from "react-hook-form";
+import Logo from "@/components/common/Logo";
+import SignInput from "@/app/(sign)/SignInput";
+
+const SignInForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<{
+    email: string;
+    password: string;
+    rememberMe: boolean;
+  }>();
+
+  const onSubmit = async (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <form className={styles.signInForm} onSubmit={handleSubmit(onSubmit)}>
+      <Logo />
+      <SignInput
+        register={register}
+        validation={{
+          required: "Required.",
+        }}
+        name="nickname"
+        type="text"
+        placeholder="Enter your nickname"
+        title="Nickname"
+      />
+      <SignInput
+        register={register}
+        validation={{
+          required: "Required.",
+          pattern: {
+            value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i,
+            message: "It's not in the form of an email.",
+          },
+        }}
+        name="email"
+        type="text"
+        placeholder="Enter your email"
+        title="Email"
+      />
+      {errors.email && (
+        <p className={styles.errorText}>{errors?.email?.message}</p>
+      )}
+
+      <SignInput
+        register={register}
+        validation={{ required: "Required." }}
+        name="password"
+        type="password"
+        placeholder="Enter your password"
+        title="Password"
+      />
+      <button className={styles.signUpButton} type="submit">
+        Sign up
+      </button>
+    </form>
+  );
+};
+
+export default SignInForm;
--- frontend/src/app/(sign)/sign-up/page.tsx
+++ frontend/src/app/(sign)/sign-up/page.tsx
@@ -1,3 +1,20 @@
+import { pickYoutubeVideoInformation } from "@/utils/config/youtubeViewer.config";
+import styles from "@/app/(sign)/sign.module.scss";
+import YoutubeViewer from "@/components/youtubeViewer/YoutubeViewer";
+import React from "react";
+import SignUpForm from "@/app/(sign)/sign-up/SignUpForm";
+
 export default function SignUp() {
-  return <div>signUp</div>;
+  const youtubeVideoInformation = pickYoutubeVideoInformation();
+  return (
+    <div className={styles.signInWrapper}>
+      <div className={styles.signInFormWrapper}>
+        <SignUpForm />
+      </div>
+      <YoutubeViewer
+        videoId={youtubeVideoInformation.id}
+        startTime={youtubeVideoInformation.startTime}
+      />
+    </div>
+  );
 }
